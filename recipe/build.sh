a175b7e01da78dbe0ae9bf26973cf540cdf07467
munechika-koyo <munechika.koyo@gmail.com>
2026-02-28 00:58:53 +0100

    Fix conditional check for build platform in build script

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -eux
 
-if [ "${build_platform}" == "osx-arm64" ]; then
+if [ "${TARGET_PLATFORM}" == "osx-arm64" ]; then
     # binary repack on Apple Silicon because build machines only support that via cross compiling right now
     mkdir -p ${PREFIX}/bin
     cp sass ${PREFIX}/bin
