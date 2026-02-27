ca1a09625f8793a00d6170ec4178cac039170656
munechika-koyo <munechika.koyo@gmail.com>
2026-02-28 00:47:04 +0100

    Fix build scripts for consistency and error handling

--- recipe/build.bat
+++ recipe/build.bat
@@ -2,9 +2,8 @@
 @REM dart pub get
 @REM dart compile exe bin\sass.dart -Dversion=%PKG_VERSION% -Ddart-version=%DART_VERSION% -o %LIBRARY_BIN%\sass.exe
 
-:: Copy Dart's license into _h_env, because the grinder task copies it
-COPY %RECIPE_DIR%\dart_license %PREFIX%\LICENSE
 dart run grinder pkg-standalone-windows-x64
+if %ERRORLEVEL% NEQ 0 exit 1
 
 cd build
 tar -xf dart-sass-%PKG_VERSION%-windows-x64.zip
--- recipe/build.sh
+++ recipe/build.sh
@@ -1,13 +1,12 @@
 #!/usr/bin/env bash
 set -eux
 
-if [ "$SUBDIR" == "osx-arm64" ]; then
-    # binary repack on Apple Silicon because build machines only support that via
-    #     cross compiling right now
-    mkdir -p $PREFIX/bin
-    cp sass $PREFIX/bin
-    cp -r src $PREFIX/bin
+if [ "$build_platform" == "osx-arm64" ]; then
+    # binary repack on Apple Silicon because build machines only support that via cross compiling right now
+    mkdir -p ${PREFIX}/bin
+    cp sass ${PREFIX}/bin
+    cp -r src ${PREFIX}/bin
 else
     dart pub get
-    dart compile exe --define="version=${PKG_VERSION}" bin/sass.dart -o $PREFIX/bin/sass
+    dart compile exe --define="version=${PKG_VERSION}" bin/sass.dart -o ${PREFIX}/bin/sass
 fi
