434849ec2e506748a53fbe923c9d1bed74c6c074
Koyo MUNECHIKA <munechika.koyo@gmail.com>
2026-03-02 16:58:45 +0100

    Refactor build.bat to streamline build process and remove commented code

--- recipe/build.bat
+++ recipe/build.bat
@@ -1,14 +1,8 @@
-@REM for /f %%i in ('python -c "import subprocess; out = subprocess.check_output(['dart', '--version']); print(out.decode().split()[3])"') do set DART_VERSION=%%i
-@REM dart pub get
-@REM dart compile exe bin\sass.dart -Dversion=%PKG_VERSION% -Ddart-version=%DART_VERSION% -o %LIBRARY_BIN%\sass.exe
-
-dart run grinder pkg-standalone-windows-x64
+dart pub get
 if %ERRORLEVEL% NEQ 0 exit 1
 
-cd build
-tar -xf dart-sass-%PKG_VERSION%-windows-x64.zip
-cd dart-sass
-COPY sass.bat %LIBRARY_BIN%\sass.bat
+dart run grinder protobuf
+if %ERRORLEVEL% NEQ 0 exit 1
 
-robocopy /E "src" "%LIBRARY_BIN%\src"
-if %ERRORLEVEL% GEQ 8 exit 1
+dart compile exe --define="version=%PKG_VERSION%" bin/sass.dart -o %LIBRARY_BIN%\sass.exe
+if %ERRORLEVEL% NEQ 0 exit 1
