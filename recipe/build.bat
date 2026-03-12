5d4a0de687ffa2005d30559816b3890d4ff90d70
munechika-koyo <munechika.koyo@gmail.com>
2026-03-12 22:37:10 +0100

    Fix path separator in build.bat for consistency

--- recipe/build.bat
+++ recipe/build.bat
@@ -4,5 +4,5 @@ if %ERRORLEVEL% NEQ 0 exit 1
 dart run grinder protobuf
 if %ERRORLEVEL% NEQ 0 exit 1
 
-dart compile exe --define="version=%PKG_VERSION%" bin/sass.dart -o %LIBRARY_BIN%\sass.exe
+dart compile exe --define="version=%PKG_VERSION%" bin\sass.dart -o %LIBRARY_BIN%\sass.exe
 if %ERRORLEVEL% NEQ 0 exit 1
